e0bccc1d3dbd1ced0df0322fbbbb45995a51e6a0
Lionel <admin@nukua.es>
2017-01-21 20:11:26 +0100

    Update cm_pi_qtpro_table.php
    spanish

--- New Files for 2.3.4 BS/catalog/includes/languages/espanol/modules/content/product_info/cm_pi_qtpro_table.php
+++ New Files for 2.3.4 BS/catalog/includes/languages/espanol/modules/content/product_info/cm_pi_qtpro_table.php
@@ -10,9 +10,9 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_TITLE', 'QTPRO Stock Table');
-  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_DESCRIPTION', 'Shows the QTPRO Stock Table on the Product Info Page.');
+  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_TITLE', 'QTPRO Tabla de Stock');
+  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_DESCRIPTION', 'Mostrar la tabla de stock (QTPRO) en la página de información del producto.');
   
-  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_HEADING', 'Currently on stock:');
-  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_PRICE', 'Price');
-  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_STOCK', 'Qty.');
+  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_HEADING', 'Actualmente en stock:');
+  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_PRICE', 'Precio');
+  define('MODULE_CONTENT_PRODUCT_INFO_QTPRO_TABLE_STOCK', 'Cantidad.');
